04bf3dc89d85faebbfeea36b4adc10565761ded0
RonaldKiprotich <protich12@gmail.com>
2020-10-31 13:56:44 +0300

    register and login forms

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Raykip254@localhost/blog'
     SECRET_KEY =os.environ.get('SECRET_KEY')
    
    
@@ -20,6 +20,7 @@ class TestConfig(Config):
     DEBUG = True
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Raykip254@localhost/blog'
     DEBUG = True
 
 config_options = {
--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
-from app import create_app
+from app import create_app,db
+from app.models import User
 from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
 
 
 
@@ -12,6 +14,8 @@ app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 
 
