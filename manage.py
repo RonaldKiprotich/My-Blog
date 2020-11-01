57e099d83bc85bc9dbda4a5d8e61ce45ded855bc
RonaldKiprotich <protich12@gmail.com>
2020-11-01 14:32:14 +0300

    debug

--- manage.py
+++ manage.py
@@ -18,11 +18,8 @@ migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
 
-
-
-
 @manager.shell
 def make_shell_context():
-    return dict(app = app,User = User )
+    return dict(app = app,db = db,User = User )
 if __name__ == '__main__':
     manager.run()
