fd45500e4aab9db46e476f4ccfe0b6c2eb3db696
RonaldKiprotich <protich12@gmail.com>
2020-11-01 14:16:04 +0300

    routing to display blogs and quotes

--- app/main/views.py
+++ app/main/views.py
@@ -11,15 +11,20 @@ def index():
     return render_template('index.html')
 
 
-@main.route('/blog/new_blog',methods= ['GET','POST'])
+@main.route('/blog/newBlog',methods = ['GET','POST'])
 @login_required
-def new_blog():
-    blogForm = BlogForm()
-    if blogForm.validate_on_submit():
-        titleBlog=blogForm.blogTitle.data
-        description = blogForm.blogDescription.data
-        newBlog = Blog(title_blog=titleBlog, description=description, user= current_user)
-        newBlog.saveBlog()
-        return redirect(url_for('main.allBlogs'))
-    title = 'New Blog'
-    return render_template('new_blogs.html', title=title, blog_form=blogForm)
+def newBlog():
+    blogForm = BlogForm()
+    if blogForm.validate_on_submit():
+        titleBlog=blogForm.blogTitle.data
+        description = blogForm.blogDescription.data
+        newBlog = Blog(title_blog=titleBlog, description=description, user= current_user)
+        newBlog.save_blog()
+        return redirect(url_for('main.allBlogs'))
+    title = 'New Blog'
+    return render_template('new_blogs.html', title=title, blog_form=blogForm)
+@main.route('/blog/allblogs', methods=['GET', 'POST'])
+@login_required
+def allBlogs():
+    blogs = Blog.get_all_blogs()
+    return render_template('blogs.html', blogs=blogs)
