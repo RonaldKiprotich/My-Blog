bbfcbc2f8bcc684c0b6dba93645216329c170ee2
RonaldKiprotich <protich12@gmail.com>
2020-11-01 14:11:41 +0300

    import blog class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from wtforms import ValidationError
+from ..models import User
 
 
 
